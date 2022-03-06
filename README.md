f004c045335a360e348c86db8cb70c4ae430a2e0
Pablo Sirera <psirera4@gmail.com>
2022-03-06 14:01:18 +0100

    config project

---
+++ README.md
@@ -0,0 +1,19 @@
+# Food Guide
+
+### Stack I Used
+
+---
+
+- Vue 3
+- Vite
+- Use the new `<script setup>` syntax
+- Tailwindcss
+- Husky
+
+For configure:
+
+- [ ] PWA
+- [ ] I18n
+- [ ] Testing (Vitest)
+- [ ] Layout system
+- [ ] unocss preset-icons
---
+++ stylelint.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-prettier',
+    'stylelint-config-recommended-vue',
+  ],
+  rules: {
+    'block-no-empty': null,
+    'block-opening-brace-space-before': 'always',
+    'color-no-invalid-hex': true,
+    'comment-empty-line-before': [
+      'always',
+      {
+        ignore: ['stylelint-commands', 'after-comment'],
+      },
+    ],
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'variants',
+          'responsive',
+          'screen',
+        ],
+      },
+    ],
+    'declaration-colon-space-after': 'always',
+    'max-empty-lines': 2,
+    'rule-empty-line-before': [
+      'always',
+      {
+        except: ['first-nested'],
+        ignore: ['after-comment'],
+      },
+    ],
+    'unit-allowed-list': ['px', 'em', 'rem', '%', 's', 'vh', 'vw', 'fr', 'seg'],
+  },
+}
