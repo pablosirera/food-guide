edebb8a6a8c65c382a05d7592ea7a9bed89649af
Pablo Sirera <psirera4@gmail.com>
2022-04-01 18:32:20 +0200

    test: update snapshot and mocking vue router

--- src/pages/__tests__/HomePage.spec.js
+++ src/pages/__tests__/HomePage.spec.js
@@ -1,6 +1,14 @@
 import { mount } from '@vue/test-utils'
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import HomePage from '@/pages/HomePage.vue'
+
+vi.mock('vue-router', () => {
+  return {
+    useRoute: () => {
+      return { path: '/' }
+    },
+  }
+})
 
 describe('Pages > HomePage', () => {
   it('should be rendered', () => {
