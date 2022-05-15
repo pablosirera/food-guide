c21f8d26c5e4713a145e2752852456561905fd1a
Pablo Sirera <psirera4@gmail.com>
2022-05-15 15:07:47 +0200

    fix: bg color auth page

--- src/pages/auth/AuthPage.vue
+++ src/pages/auth/AuthPage.vue
@@ -1,12 +1,12 @@
 <script setup></script>
 
 <template>
-  <div class="h-full overflow-x-hidden flex flex-col">
+  <div class="h-full overflow-x-hidden flex flex-col bg-secondary">
     <section class="py-20">
       <h1 class="text-center text-3xl font-bold">Food Guide</h1>
     </section>
 
-    <section class="login-body flex justify-center h-full bg-gray-700">
+    <section class="login-body flex justify-center h-full bg-base-100">
       <div class="h-full w-2/5 flex flex-col justify-around">
         <router-view></router-view>
       </div>
